ea1a285114ae3e6df054f3c625cb135919dc3426
chinwe2020 <cfwatkinsp@gmail.com>
2020-12-14 14:39:53 -0600

    front and back running

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import './App.css';
 import './';
 
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const port = process.env.PORT || 4001;
-const index = require('./routes/index');
-
-const app = express();
-app.use(index);
-
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const getApiAndEmit = "TODO";
 
 function App() {
   return (
